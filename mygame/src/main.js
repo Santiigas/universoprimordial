809015db573bf088b169a4fb3b0696800c413002
Santiigas <santiagosof@gmail.com>
2024-01-18 11:31:30 -0300

    +

--- mygame/src/main.js
+++ mygame/src/main.js
@@ -2,12 +2,11 @@ import kaboom from "kaboom"
 
 //npm run dev
 
+
 kaboom()
 
 //carregando imagen do personagem 
 loadSprite("bean", "sprites/bean.png")
-loadSprite("cenario", "sprites/cenario.png")
-
 
 
 //conceitos de cena. O que vai acontecer se algo acontecer
@@ -23,7 +22,7 @@ scene("game", () => {
 		//pegando a imagem e joga na teal
 		sprite("bean"),
 		//posicao na tela
-		pos(100, 100),
+		pos(80, 180),
 		//area de colisao
 		area(),
 		//significa que é um corpo fisico e que responde a uma gravidade
@@ -41,16 +40,71 @@ scene("game", () => {
 
 
 	// adiciona uma plataforma(chão)
-	const chao = add([
-		sprite("cenario"),
-		scale(1),
+	add([
+		//cria uma retangulo
+		rect(width(), 48),
 		//posicao do retangulo
-		pos(0, -100),
+		pos(0, height() - 48),
+		//sombra/contorno
+		outline(4),
 		//pode ocorrer colisoes nele
 		area(),
 		//responde a uma gravidade e "isStattic: true" nao pode se mover
 		body({ isStatic: true }),
+		color(127, 200, 255),
 	])
+
+
+	//funcao para colocar a arvore aparecendo em intervalos de tempos aletarorios
+	function spawnTree() {
+		// adiciona arvore
+		add([
+			//cria um retangulo com as seguintes dimensoes
+			//rand dizendo que a altura pode varias de 24 a 64
+			rect(48, rand(24, 64)),
+			//area de colisao(para nao bater nas coisas)
+			area(),
+			outline(4),
+			pos(width(), height() - 48),
+			//ponto de origem
+			anchor("botleft"),
+			//ancorado do lado esquerdo
+			color(255, 180, 255),
+			//ele vai se mover para a esquerva a 240 px por segundo
+			move(LEFT, 240),
+			//definindo um nome/tag para o elemento
+			"tree",
+		]);
+			wait(rand(0.5, 1.5), () => {
+			spawnTree();
+		});
+	}
+
+	spawnTree();
+
+	//contador de pontos
+	let score = 0;
+	const scoreLabel = add([
+    	text(score),
+    	pos(24, 24)
+	])
+
+	// a cada frame que passar ele add um a pontuacao
+	onUpdate(() => {
+    	score++;
+    	scoreLabel.text = score;
+	});
+
+	//quando o bean colidir com a arvore(tree) vai add alguma coisa
+	bean.onCollide("tree", () => {
+		addKaboom(bean.pos);
+		//faz a tela tremer
+		//ali ja pode colocar uma variavel que toda vez que um inimigo bater nele perde uma vida
+		shake();
+		//quando o bee colidir vai mudar de cena
+		go("lose"); // go to "lose" scene here
+	});
+
 
 })
 
